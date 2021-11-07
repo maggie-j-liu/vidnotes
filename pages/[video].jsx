244b6e9183947a555d61e23708091a0b6adedfe4
maggie-j-liu <maggie.j.liu@gmail.com>
2021-11-07 12:50:01 -0800

    markdown

--- pages/[video].jsx
+++ pages/[video].jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import { FiSettings } from "react-icons/fi";
+import ReactMarkdown from "react-markdown";
 import useSettings from "../components/SettingsContext";
 import SettingsModal from "../components/SettingsModal";
+import remarkBreaks from "remark-breaks";
 
 const Video = ({ video }) => {
-  const [notesHidden, setNotesHidden] = useState(true);
+  const [notesHidden, setNotesHidden] = useState(false);
   const [notes, setNotes] = useState("");
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const [previewMode, setPreviewMode] = useState(false);
   const { color, opacity } = useSettings();
   const notesRef = useRef(null);
   const buttonRef = useRef(null);
@@ -52,18 +55,41 @@ const Video = ({ video }) => {
         } absolute pointer-events-none inset-0 w-full h-full ${color}`}
         style={{ opacity: `${opacity}%` }}
       />
+      {!notesHidden && (
+        <button
+          onClick={() => setPreviewMode((p) => !p)}
+          className="z-20 px-2 py-1 rounded-md bg-white absolute left-4 bottom-6"
+        >
+          {previewMode ? "Edit" : "Preview"}
+        </button>
+      )}
       <div
         className={`${
           notesHidden ? "hidden" : ""
         } w-full absolute inset-0 py-12 printable`}
       >
         <textarea
-          placeholder="> Start typing..."
-          className="z-10 printable focus:outline-none bg-transparent mx-auto w-full max-w-prose block min-h-full my-12 placeholder-gray-700 text-2xl"
+          placeholder={`→ Start typing your notes (markdown is supported)\nPress escape to close the notes overlay.`}
+          className={`${
+            previewMode ? "hidden" : "block"
+          } z-10 focus:outline-none bg-transparent mx-auto w-full max-w-prose min-h-full my-12 placeholder-gray-700 text-2xl`}
           value={notes}
           onChange={(e) => stickySetNotes(e.target.value)}
           ref={notesRef}
         />
+
+        <div
+          className={`${
+            previewMode ? "" : "hidden"
+          } mx-auto w-full markdown printable max-w-prose text-2xl my-12`}
+        >
+          <ReactMarkdown
+            remarkPlugins={[remarkBreaks]}
+            className="break-words prose prose-blue"
+          >
+            {notes}
+          </ReactMarkdown>
+        </div>
       </div>
 
       <div className="bg-primary blur-md absolute bottom-20 right-4 p-1.5 rounded-full">
